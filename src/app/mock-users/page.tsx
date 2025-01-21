62051cf6825c108a71786584fff469d83bb63df9
d3vkk <donaldkk22@yahoo.com>
2025-01-21 15:37:43 +0300

    feat: Server action for create

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -1,3 +1,5 @@
+import { revalidatePath } from "next/cache";
+
 type MockUser = {
   id: number;
   name: string;
@@ -6,8 +8,37 @@ type MockUser = {
 export default async function mockUsers() {
   const response = await fetch("https://mockapi.io/users");
   const users = await response.json();
+
+  async function addUser(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const response = await fetch("https://mockapi.io/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    });
+    const newUser = await response.json();
+    console.log(newUser);
+
+    // refresh page
+    revalidatePath("/mock-users");
+  }
+
   return (
     <div className="grid grid-cols-4 gap-4 py-10">
+      <form className="mb-4" action={addUser}>
+        <input className="border p-2 mr-2" type="text" name="name" required />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Add user
+        </button>
+      </form>
       {users.map((user: MockUser) => (
         <div
           key={user.id}
